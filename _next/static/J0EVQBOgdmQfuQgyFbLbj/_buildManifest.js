281fbaa6957eb72f8313980424a92d6507d748c7
syahirAmali <52483029+syahirAmali@users.noreply.github.com>
2022-03-26 06:42:00 +0000

    Deploying to gh-pages from @ syahirAmali/syahirAmali.github.io@d096d4e080abb7c96565b71e1762a16a7cc51c94 🚀

---
+++ _next/static/J0EVQBOgdmQfuQgyFbLbj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/230-4c9cef05451ae8d2.js","static/chunks/pages/index-7dbbacfeb5c70c75.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
