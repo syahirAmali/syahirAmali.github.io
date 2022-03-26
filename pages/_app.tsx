1d1fff3451fd36d3531f1713c66b4b2607eb8071
Syahir Amali <syahiramali@gmail.com>
2022-03-27 03:25:08 +0800

    app changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import '@fontsource/roboto/400.css'
-
+import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from "../lib/theme"
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+        <title>Syahir Amali</title>
+        <link rel="icon" href="/cat.svg" />
+    </Head>
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from 'next/head'
 import Home from './home';
 
 export default function main() {
 
   return (
     <div>
-      <Head>
-        <title>Syahir Amali</title>
-        <link rel="icon" href="/cat.svg" />
-      </Head>
       <Home />
     </div>
   )
